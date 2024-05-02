ffc2a92826b42464425b24e5437cae097895f808
jeel09 <vachhanijeel50@gmail.com>
2024-05-02 11:39:51 +0530

    signout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 export default function ProfilePage() { 
+    const router = useRouter();
+    const logout = async () => { 
+        try {
+            const response = await axios.get('https://localhost:7098/api/UserAPI/Signout');
+            console.log("logout success", response.data);
+            toast.success("Logout success");
+            router.push("/login");
+
+            document.cookie = "token=; expires=" + new Date().toUTCString() + "; path=/;";
+        }
+        catch (error: any) {
+            console.log("logout failed", error.message);
+            toast.error("Logout failed");
+        }
+    }
+
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile Page</h1>
+            <hr />
+            <button
+                onClick={logout}
+                className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >LogOut</button>
         </div>
     );
 }
